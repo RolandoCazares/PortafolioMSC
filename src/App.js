3e88ac3cbfd2c41ae9daae185ddc6f9ede43b233
Huawei Rolando <rolando_99-2011@hotmail.com>
2022-09-04 14:17:51 -0700

    se agrego home

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import NavbarNav from './components/Navbar';
-import Sidebar from './components/Sidebar';
+import Home from "../src/components/Home/home"
+import NavbarNav from './components/Home/Navbar';
+import Sidebar from './components/Home/Sidebar';
 import Unidad1 from './components/Content/Unidad1'
 import Unidad2 from './components/Content/Unidad2'
 import Unidad3 from './components/Content/Unidad3'
 import Unidad4 from './components/Content/Unidad4'
 import Unidad5 from './components/Content/Unidad5'
+import Login from './components/Login/Login';
+
 
 import './App.scss';
 
 function App() {
   return (
       <Router className="App">
-          <NavbarNav/>
-            <div className='flex'>
-              <Sidebar/>
-                <div className='content'>
-                  <Routes>
-                    <Route path="/unidad1" element={<Unidad1/>}/>
-                    <Route path="/unidad2" element={<Unidad2/>}/>
-                    <Route path="/unidad3" element={<Unidad3/>}/>
-                    <Route path="/unidad4" element={<Unidad4/>}/>
-                    <Route path="/unidad5" element={<Unidad5/>}/>
-                  </Routes>
-                </div>
-            </div>
-        
+        <Routes>
+            <Route path="/" element={<Login/>}/>
+              <Route path="/unidad1" element={<Home page={<Unidad1/>}/>}/>
+              <Route path="/unidad2" element={<Home page={<Unidad2/>}/>}/>
+              <Route path="/unidad3" element={<Home page={<Unidad3/>}/>}/>
+              <Route path="/unidad4" element={<Home page={<Unidad4/>}/>}/>
+              <Route path="/unidad5" element={<Home page={<Unidad5/>}/>}/>
+        </Routes>
       </Router>
-        
-    
-      
   );
 }
 
