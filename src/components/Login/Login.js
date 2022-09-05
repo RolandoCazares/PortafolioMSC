f399abb195a3698f5ff4662c472818f7e0845e3e
Huawei Rolando <rolando_99-2011@hotmail.com>
2022-09-04 20:17:10 -0700

    validacion

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,5 @@
-import { useContext, useRef } from "react";
-import { Navigate } from "react-router-dom";
 import styles from "./Login.module.css";
-import { makeStyles } from "@mui/styles";
-import { Link } from "react-router-dom"
-import fondo from "../../assets/images/ingenieria-sistemascomputacionales.jpg"
+import { makeStyles } from "@mui/styles";import fondo from "../../assets/images/ingenieria-sistemascomputacionales.jpg"
 import Candado from "../../assets/images/giphy.gif"
 import { useNavigate } from "react-router-dom";
 
@@ -19,15 +15,26 @@ const useStyles = makeStyles((theme) => ({
 
 
 
-function Login() {
-  const classes = useStyles();
 
-  let navigate = useNavigate(); 
-  const routeChange = () =>{ 
-    let path = `/home`; 
-    navigate(path);
-  }
+function Login() {
+    
+    const classes = useStyles();
+    let navigate = useNavigate();
   
+        function validar(){
+          let usuario="GUILLEMU";
+          let contrasena="MSC2022";
+          let x = document.form.uname.value;
+          let y = document.form.psw.value;
+          if(usuario === x && contrasena === y){
+            let path = `/home`; 
+            navigate(path);
+          }
+          else{
+            alert("Usuario y/o contraseña incorrectos")
+          }
+        }
+
   return (
     <>
      <div className={classes.root}>
@@ -37,33 +44,31 @@ function Login() {
         </div>
         <h6 className={styles.message}>On this site you can see rolando's jobs and tasks. Please log in.</h6>
         <img src={Candado} className={styles.candado}/>
-        <label htmlFor="uname">
-          <b>Username</b>
-        </label>
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="Enter Username"
-          name="uname"
-          required
-          /* ref={usernameRef} */
-        />
-        <label htmlFor="psw">
-          <b>Password</b>
-        </label>
-        <input
-          className={styles.input}
-          type="password"
-          placeholder="Enter Password"
-          name="psw"
-          required
-          /* ref={passwordRef} */
-        />
-        <button
+         {<form name="form" action="INICIO SESION">
+          <label>
+                <b>Username</b>
+              </label>
+              <input
+                className={styles.input}
+                type="text"
+                placeholder="Enter Username"
+                name="uname"
+              />
+              <label htmlFor="psw">
+                <b>Password</b>
+              </label>
+              <input
+                className={styles.input}
+                type="password"
+                placeholder="Enter Password"
+                name="psw"
+              />
+          </form>}
+
+        <button id="inicio"
           className={styles.login}
-          onClick= {
-            routeChange
-          }
+          onClick={() => {
+              return validar()}}
         >
           Login
         </button>
