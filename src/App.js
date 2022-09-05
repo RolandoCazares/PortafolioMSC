12268e44da551eebc888bcefc9cd8bcd420f2ce2
Huawei Rolando <rolando_99-2011@hotmail.com>
2022-09-04 22:42:00 -0700

    se borro algo incecesario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from "../src/components/Home/home"
-import Unidad1 from './components/Content/Unidad1'
+import Unidad1 from './components/Content/Unidad1/Unidad1.js'
 import Unidad2 from './components/Content/Unidad2'
 import Unidad3 from './components/Content/Unidad3'
 import Unidad4 from './components/Content/Unidad4'
 import Unidad5 from './components/Content/Unidad5'
 import Login from './components/Login/Login';
-import Inicio from './components/Init/Init';
 import HomeInit from './components/Init/HomeInit';
 
 
@@ -17,7 +16,7 @@ function App() {
       <Router className="App">
         <Routes>
             <Route path="/" element={<Login/>}/>
-              <Route path="/home" element= {<HomeInit page={<Inicio/>}/>}>
+              <Route path="/home" element= {<HomeInit/>}>
               <Route path="/home/unidad1" element={<Home page={<Unidad1/>}/>}/>
               <Route path="/home/unidad2" element={<Home page={<Unidad2/>}/>}/>
               <Route path="/home/unidad3" element={<Home page={<Unidad3/>}/>}/>
